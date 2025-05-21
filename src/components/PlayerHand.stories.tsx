dceba8751ae5661e1838a5abaa9e5585275ddf52
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2025-05-21 17:24:15 +0200

    Create Storybook stories for PlayerHand component

---
+++ src/components/PlayerHand.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import App from '../App';
+
+export default {
+  title: 'App',
+  component: App,
+} as Meta;
+
+const Template: Story = () => <App />;
+
+export const Default = Template.bind({});
+Default.args = {};
