025d82e71713ce77925bc50f86674c60fa9eb0d4
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2025-05-22 13:43:43 +0200

    Integrate StakesDisplay component

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,13 +1,16 @@
 import PlayerHand from '../components/PlayerHand';
+import StakesDisplay from '../components/StakesDisplay';
 import { usePlayerHand } from '../services/playerHandProvider';
 
 export default function Game() {
   const initialCards = usePlayerHand();
+  
   return (
     <div style={{ padding: 20 }}>
       <h2>Game Page</h2>
       <p>Here we will play Truco against the AI.</p>
       <PlayerHand initialCards={initialCards} />
+      <StakesDisplay />
     </div>
   );
 }
