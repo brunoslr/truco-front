7fc00f365945432126415b78ddb7010f9d0f5503
Bruno Samuel Leal Rocha <brunosamuellr@gmail.com>
2025-05-22 13:37:39 +0200

    Integrate StakesDisplay component into Game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,13 +1,22 @@
+import React, { useState } from 'react';
 import PlayerHand from '../components/PlayerHand';
+import StakesDisplay from '../components/StakesDisplay';
 import { usePlayerHand } from '../services/playerHandProvider';
 
-export default function Game() {
+const Game: React.FC = () => {
+  const [stakes, setStakes] = useState(2);
   const initialCards = usePlayerHand();
+
   return (
     <div style={{ padding: 20 }}>
+      <h1>Truco Mineiro Game</h1>
+      <StakesDisplay stakes={stakes} />
       <h2>Game Page</h2>
       <p>Here we will play Truco against the AI.</p>
       <PlayerHand initialCards={initialCards} />
+      {/* Add logic here to change the stakes value based on game events */}
     </div>
   );
-}
+};
+
+export default Game;
